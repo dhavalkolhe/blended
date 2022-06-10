83a2b0440c27faa90eca6054fd8793aba42fa6af
Dhaval Kolhe <72658475+dhavalkolhe@users.noreply.github.com>
2022-06-10 19:32:18 +0530

    Styling Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import { useRouter } from "next/router";
-// import Image from "next/image";
-// import styles from "../styles/Home.module.css";
 
 function RedirectPage({ ctx }) {
   const router = useRouter();
